9bc393577503d45487e75c1e9e24f2aed27d6a84
Cassas Tomas <tomascassas@gmail.com>
2022-04-07 19:17:28 -0300

    Punto 1 y agrego carpeta vscode al gitignore

---
+++ tp4_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+#define M 5
+
+int main() {
+    char * Buff = (char *) malloc(100*sizeof(char));
+    char * nombres[M];
+
+    int i;
+    printf("Ingrese 5 nombres:\n");
+    for(i = 0; i < M; i++) {
+        gets(Buff);
+        nombres[i] = (char *) malloc((strlen(Buff) + 1) * sizeof(char));
+        strcpy(nombres[i], Buff);
+    }
+
+    for(i = 0; i < M; i++) {
+        printf("\nNombre %d: %s\n", i + 1, nombres[i]);
+    }
+
+    free(Buff);
+    free(i); //No se si debería hacer free por cada variable que tenga
+
+    return 0;
+}
